9204d9fec45732f3611f8bf36086b304f15ba17e
Zidane Innis <zidaneinnis@gmail.com>
2022-05-29 21:48:18 -0500

    Building a Slider Component: Part 1

--- script.js
+++ script.js
@@ -204,7 +204,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 });
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  // section.classList.add('section--hidden');
 });
 
 // Lazy loading images
@@ -230,10 +230,53 @@ const loadImg = function (entries, observer) {
 const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
   threshold: 0,
-  rootMargin: '+200px',
+  rootMargin: '200px',
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
+
+// Slider
+
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+// const slider = document.querySelector('.slider');
+// slider.style.transform = `scale(0.4) translateX(-1200px)`;
+// slider.style.overflow = `visible`;
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+
+goToSlide(0);
+
+// Next Slide
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else curSlide++;
+
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else curSlide--;
+  goToSlide(curSlide);
+};
+
+btnLeft.addEventListener('click', prevSlide);
+
+// curSlide = 1; -100%, 0%, 100%, 200%
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
